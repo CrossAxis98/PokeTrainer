9b3fe190754216a2d5bf3a0cb7549d3c313313a7
CrossAxis98 <roberto2611@gmail.com>
2022-11-25 18:23:28 +0100

    Added arrow back to StatsScreen

--- app/src/main/java/com/example/poketrainer/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/example/poketrainer/screens/stats/StatsScreen.kt
@@ -1,17 +1,20 @@
 package com.example.poketrainer.screens.stats
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.poketrainer.navigation.PokeTrainerScreens
 import com.example.poketrainer.screens.home.HomeViewModel
 import com.google.firebase.auth.FirebaseAuth
 
@@ -25,18 +28,40 @@ fun StatsScreen(navController: NavController, homeViewModel: HomeViewModel = hil
     val pokemonsToCatchList = pokemonsList.filter { pokemonBasicInfo ->
         pokemonBasicInfo.isMarkedAsWannaCatch && !pokemonBasicInfo.isMarkedAsCaught
     }
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(start = 10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Start
-    ) {
-        Text(text = name, style = MaterialTheme.typography.h5)
-        val pokemonCaughtEndText = if ( pokemonsCaughtList.size == 1) "pokemon" else "pokemons"
-        Text(text = "You have ${pokemonsCaughtList.size} $pokemonCaughtEndText.", style = MaterialTheme.typography.h6)
-        val pokemonWannaCatchEndText = if ( pokemonsToCatchList.size == 1) "pokemon" else "pokemons"
-        Text(text = "Wanna catch ${pokemonsToCatchList.size} $pokemonWannaCatchEndText.", style = MaterialTheme.typography.h6)
-
+    Scaffold(topBar = {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Arrow back",
+            modifier = Modifier
+                .offset(10.dp, 20.dp)
+                .size(28.dp)
+                .clickable {
+                    navController.navigate(PokeTrainerScreens.HomeScreen.name) {
+                        popUpTo(0)
+                    }
+                }
+        )
+    }) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = name, style = MaterialTheme.typography.h5)
+            val pokemonCaughtEndText = if (pokemonsCaughtList.size == 1) "pokemon" else "pokemons"
+            Text(
+                text = "You have ${pokemonsCaughtList.size} $pokemonCaughtEndText.",
+                style = MaterialTheme.typography.h6
+            )
+            val pokemonWannaCatchEndText =
+                if (pokemonsToCatchList.size == 1) "pokemon" else "pokemons"
+            Text(
+                text = "Wanna catch ${pokemonsToCatchList.size} $pokemonWannaCatchEndText.",
+                style = MaterialTheme.typography.h6
+            )
+        }
     }
-
 }

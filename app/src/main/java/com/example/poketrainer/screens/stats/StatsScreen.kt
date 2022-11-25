9b148e0342af1d2d4d8bb519b67c75a99d71c4c7
CrossAxis98 <roberto2611@gmail.com>
2022-11-25 18:06:26 +0100

    Added stats to StatsScreen

--- app/src/main/java/com/example/poketrainer/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/example/poketrainer/screens/stats/StatsScreen.kt
@@ -3,24 +3,40 @@ package com.example.poketrainer.screens.stats
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import com.example.poketrainer.screens.home.HomeViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun StatsScreen(navController: NavController) {
+fun StatsScreen(navController: NavController, homeViewModel: HomeViewModel = hiltViewModel()) {
     val name: String = FirebaseAuth.getInstance()
         .currentUser!!.email!!.split("@")[0]
         .replaceFirstChar { it.uppercase() }
+    val pokemonsList by remember { homeViewModel.pokemonList }
+    val pokemonsCaughtList = pokemonsList.filter { pokemonBasicInfo -> pokemonBasicInfo.isMarkedAsCaught  }
+    val pokemonsToCatchList = pokemonsList.filter { pokemonBasicInfo ->
+        pokemonBasicInfo.isMarkedAsWannaCatch && !pokemonBasicInfo.isMarkedAsCaught
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top
+        modifier = Modifier.fillMaxSize().padding(start = 10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
     ) {
-        Text(text = name)
+        Text(text = name, style = MaterialTheme.typography.h5)
+        val pokemonCaughtEndText = if ( pokemonsCaughtList.size == 1) "pokemon" else "pokemons"
+        Text(text = "You have ${pokemonsCaughtList.size} $pokemonCaughtEndText.", style = MaterialTheme.typography.h6)
+        val pokemonWannaCatchEndText = if ( pokemonsToCatchList.size == 1) "pokemon" else "pokemons"
+        Text(text = "Wanna catch ${pokemonsToCatchList.size} $pokemonWannaCatchEndText.", style = MaterialTheme.typography.h6)
+
     }
 
 }
